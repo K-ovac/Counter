dc83c426667345fef1270adc57db7217b24fbbd6
K-ovac <biu.jay1@yandex.ru>
2025-04-30 20:58:23 +0300

    Написан код приложения

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var tapButton: UIButton!
+    @IBOutlet var scoreLabel: UILabel!
+    
+    var score: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        scoreLabel.text = "Значение счетчика: \(score)"
+        
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapper(_ sender: Any) {
+        score += 1
+        scoreLabel.text = "Значение счетчика: \(score)"
+    }
 }
 
